46e0896775fc2f11462ed5f4a23bc93598cf98e1
iFrostizz <fgg67@hotmail.fr>
2022-05-18 11:09:34 +0200

    usage of styled components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,17 +1,31 @@
 import Header from "./Header";
 import React from "react";
-import '../styles.css'
+import styled from 'styled-components'
+
+const ContentWrapper = styled.div`margin-top: 50px;
+  margin-left: 100px;`
+const SmallTitle = styled.div`font-size: 2em;
+  margin-left: 100px;`
+const BigTitle = styled.div`font-size: 3em;
+  margin-left: 100px;`
+const ProjectWrapper = styled.div`margin-top: 50px;
+  margin-left: 70px;`
+const ListWrapper = styled.div`margin-left: 70px;`
+const SmallEmoji = styled.img`width: 30px;
+  height: 30px;
+  position: absolute;
+  margin-top: 10px;`
 
 const Content = () => {
 	
 	return (
 		<>
 			<Header/>
-			<div className="content_wrapper">
-				<div className="big_title">My projects</div>
-				<div className="projects_wrapper">
-					<div className="small_title">Octoprint projects</div>
-					<div className="list_wrapper">
+			<ContentWrapper>
+				<BigTitle>My projects</BigTitle>
+				<ProjectWrapper>
+					<SmallTitle>Octoprint projects</SmallTitle>
+					<ListWrapper>
 						<ul>
 							<li><a href="https://github.com/iFrostizz/OctoPrint-EasyServo" target="_blank"
 										 rel="noreferrer"><code>Easyservo</code></a>:
@@ -23,9 +37,9 @@ const Content = () => {
 								calibrate your 3D printer in few clicks
 							</li>
 						</ul>
-					</div>
-					<div className="small_title">Web3 projects</div>
-					<div className="list_wrapper">
+					</ListWrapper>
+					<SmallTitle>Web3 projects</SmallTitle>
+					<ListWrapper>
 						<ul>
 							<li><a href="https://cryptobrokesquad.xyz/" target="_blank" rel="noreferrer"><code>Cryptobroke</code></a>:
 								NFT's for the brokest
@@ -37,18 +51,18 @@ const Content = () => {
 								an NFT marketplace running on the Velas blockchain
 							</li>
 						</ul>
-					</div>
-					<div className="small_title">Building &nbsp;<img src={process.env.PUBLIC_URL + '/building.png'}
-																													 className="small_emoji" /*alt="building"*//></div>
-					<div className="list_wrapper">
+					</ListWrapper>
+					<SmallTitle>Building &nbsp;<SmallEmoji src={process.env.PUBLIC_URL + '/building.png'}
+																								 className="small_emoji" /*alt="building"*//></SmallTitle>
+					<ListWrapper>
 						<ul>
 							<li><a href="https://github.com/iFrostizz/fether" target="_blank" rel="noreferrer"><code>Fether</code></a>:
 								Hack and solve Ethernaut challenges using foundry
 							</li>
 						</ul>
-					</div>
-				</div>
-			</div>
+					</ListWrapper>
+				</ProjectWrapper>
+			</ContentWrapper>
 		</>
 	)
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,19 +3,26 @@ import {ThemeContext} from "../contexts/ColorMode";
 import {IconButton} from "@mui/material";
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
-import '../styles.css'
+import styled from 'styled-components'
+
+const MainTitle = styled.div`text-align: center;
+margin-left: 100px;
+font-size: 4em;`
+const NightToggle = styled.span`float: right;
+  margin-right: 20px;`
 
 const Header = () => {
 	const {isDarkMode, toggleDarkMode} = useContext(ThemeContext)
 	
 	return (
 		<header>
-			<a href="." className="website_title">franfran.dev</a>
-			<span className="night_toggle">
-				<IconButton sx={{ml: 1}} onClick={() => toggleDarkMode()} color="inherit">
-					{isDarkMode ? <Brightness7Icon/> : <Brightness4Icon/>}
-				</IconButton>
-			</span>
+			<MainTitle><a href=".">franfran.dev</a>
+				<NightToggle>
+					<IconButton sx={{ml: 1}} onClick={() => toggleDarkMode()} color="inherit">
+						{isDarkMode ? <Brightness7Icon/> : <Brightness4Icon/>}
+					</IconButton>
+				</NightToggle>
+			</MainTitle>
 		</header>
 	)
 }
